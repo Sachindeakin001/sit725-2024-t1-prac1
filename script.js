2f6c74417fba66556999253c1751480551d0c5e7
Sachindeakin001 <162239234+Sachindeakin001@users.noreply.github.com>
2024-03-06 12:20:59 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("heading").innerText = "Option 1 Selected";
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+    document.getElementById("heading").innerText = "Option 2 Selected";
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+    document.getElementById("heading").innerText = "Option 3 Selected";
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+    document.getElementById("heading").innerText = "Option 4 Selected";
+});
